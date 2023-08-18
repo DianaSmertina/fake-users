f8310338a18d33f8ed8d23877a9f2e4a68cfd062
DianaSmertina <am.dianasmertina@gmail.com>
2023-08-18 14:19:34 +0400

    feat: add downloading csv

--- app/src/components/usersTable/UsersTable.tsx
+++ app/src/components/usersTable/UsersTable.tsx
@@ -2,6 +2,7 @@ import { Table } from "react-bootstrap";
 import InfiniteRows from "./InfiniteRows";
 import { useEffect, useMemo, useState } from "react";
 import { IUser, RandomUsers } from "../../RandomUsers/RandomUsers";
+import { CSVLink } from "react-csv";
 
 interface IUsersTableProps {
     region: string;
@@ -33,32 +34,40 @@ function UsersTable({ region, mistakes, seed }: IUsersTableProps) {
     }, [region, mistakes, seed]);
 
     const data = useMemo(() => users, [users]);
+    const tableData = data?.map((user, i) => [i + 1, ...Object.values(user)]) || [];
+    const csvData = [
+        ["№", "ID", "Full name", "Address", "Phone number"],
+        ...tableData
+    ];
 
     return (
-        <InfiniteRows getMoreUsers={getMoreUsers} users={data}>
-            <Table striped bordered hover>
-                <thead>
-                    <tr>
-                        <th>№</th>
-                        <th>ID</th>
-                        <th>Full name</th>
-                        <th>Address</th>
-                        <th>Phone number</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {users?.map((user, i) => (
-                        <tr key={user.userId}>
-                            <td>{i + 1}</td>
-                            <td>{user.userId}</td>
-                            <td>{user.name}</td>
-                            <td>{user.address}</td>
-                            <td>{user.phone}</td>
+        <>
+            <CSVLink data={csvData}>Download CSV</CSVLink>
+            <InfiniteRows getMoreUsers={getMoreUsers} users={data}>
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                            <th>№</th>
+                            <th>ID</th>
+                            <th>Full name</th>
+                            <th>Address</th>
+                            <th>Phone number</th>
                         </tr>
-                    ))}
-                </tbody>
-            </Table>
-        </InfiniteRows>
+                    </thead>
+                    <tbody>
+                        {users?.map((user, i) => (
+                            <tr key={user.userId}>
+                                <td>{i + 1}</td>
+                                <td>{user.userId}</td>
+                                <td>{user.name}</td>
+                                <td>{user.address}</td>
+                                <td>{user.phone}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </Table>
+            </InfiniteRows>
+        </>
     );
 }
 
