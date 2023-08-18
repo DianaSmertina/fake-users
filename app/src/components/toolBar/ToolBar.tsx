b1bd366e3897bca7d33b396976c56000c68a8401
DianaSmertina <am.dianasmertina@gmail.com>
2023-08-18 15:59:58 +0400

    feat: change styles

--- app/src/components/toolBar/ToolBar.tsx
+++ app/src/components/toolBar/ToolBar.tsx
@@ -1,4 +1,3 @@
-import "./ToolBar.css";
 import { ChangeEvent, Dispatch, SetStateAction } from "react";
 import { Button, Container } from "react-bootstrap";
 import Form from "react-bootstrap/Form";
